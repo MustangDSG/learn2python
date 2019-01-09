bf070aad935da836c55119f61b01f04344f5d69b
nquidox <40396820+nquidox@users.noreply.github.com>
2019-01-09 17:49:30 +0300

    Chapter 5 tasks + addition

---
+++ Bill Lubanovic/chapter-5.py
@@ -0,0 +1,84 @@
+ #Bill Lubanovic - Introducing Python chapter 5 tasks.
+
+divider="-------------------------------------------------------------"
+print("Вывод результатов упражнений к 5 главе.")
+print(divider)
+
+#Задание 1.
+'''
+Создайте файл, который называется zoo.py В нем объявите функцию hours(),
+которая выводит на экран строку 'Open 9-5 daily.' Далее используйте
+интерактивный интерпретатор, чтобы импортировать модуль zoo и вызвать его
+функцию hours().
+'''
+
+print('Импорт из файла.')
+import zoo
+zoo.hours()
+print(divider)
+
+#Задание 2.
+'''
+В интерактивном интерпретаторе импортируйте модуль zoo под именем menagerie
+и вызовите его функцию hours()
+'''
+
+print('Импорт из файла c переименовкой.')
+import zoo as menagerie
+menagerie.hours()
+print(divider)
+
+#Задание 3.
+'''
+Оставаясь в интерпретаторе, импортируйте непосредственно функцию hours() из
+модуля zoo и вызовите ее.
+'''
+
+print('Импорт конкретной функции.')
+from zoo import hours
+hours()
+print(divider)
+
+#Задание 4.
+'''
+Импортируйте функцию hours() под именем info и вызовите ее.
+'''
+
+print('Импорт конкретной функции c переименовкой.')
+from zoo import hours as info
+info()
+print(divider)
+
+#Задание 5.
+'''
+Создайте словарь с именем plain, содержащий пары ключ-значение 'a':1, 'b':2, 'c':3,
+а затем выведите его на экран
+'''
+
+plain={"a" : 1, "b" : 2, "c" : 3,}
+print('Простой словарь.', plain)
+print(divider)
+
+#Задание 6.
+'''
+Создайте OrderedDict с именем fancy из пар ключ-значение, приведенных в задании 5,
+и выведите его на экран. Изменился ли порядок ключей?
+'''
+
+from collections import OrderedDict
+fancy=OrderedDict([('a', 1), ('b', 2), ('c', 3)])
+fancy2=OrderedDict(plain)
+print('Упорядоченный словарь.', fancy, fancy2)
+print(divider)
+
+#Задание 7.
+'''
+Создайте defaultdict с именем dict_of_lists и передайте ему аргумент list.
+Создайте список dict_of_lists['a'] и присоедините к нему значение
+'something for a' за одну операцию. Выведите на экран dict_of_lists['a'].
+'''
+
+from collections import defaultdict
+dict_of_lists=defaultdict(list)
+dict_of_lists['a'] = 'something for a'
+print(dict_of_lists)
