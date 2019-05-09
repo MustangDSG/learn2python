73136090dda379b83474ce29ea2eef9e81585460
Alexander Serov <alexander.serov@protonmail.com>
2019-05-09 14:29:36 +0300

    Part 10, fixes on stream.

--- Bill Lubanovic/chapter-10.py
+++ Bill Lubanovic/chapter-10.py
@@ -1,4 +1,4 @@
- #Bill Lubanovic - Introducing Python chapter 10 tasks.
+#Bill Lubanovic - Introducing Python chapter 10 tasks.
 
 divider="-------------------------------------------------------------"
 print("Вывод результатов упражнений к 10 главе.")
@@ -6,9 +6,9 @@ print(divider)
 
 # Задание 1.
 # Запишите текущие дату и время как строку в текстовый файл today.txt.
+# если написать просто 'import datetime', то скрипт будет выдавать ошибку
 
 from datetime import datetime
-#если написать просто 'import datetime', то скрипт будет выдавать ошибку
 
 with open('today.txt', 'wt') as today_file:
 	current_date=str(datetime.now())
@@ -31,7 +31,6 @@ get_date = datetime.strptime(today_string, '%Y-%m-%d %H:%M:%S.%f')
 print("3) Преобразуем строку в дату по шаблону: ", get_date)
 print(divider)
 
-
 # Задание 4.
 # Выведите на экран список файлов текущего каталога.
 
@@ -39,20 +38,19 @@ import os
 
 print("4) Вывод списка файлов и папок текущего каталога:")
 print("Текущий каталог:", os.getcwd())
+
 # Далее 2 варианта вывода информации:
 # Первый, вывод через запятую сплошной строкой:
-
 # print("Содержимое каталога:\n", os.listdir(path='.'))
-
 # Второй, построчным списком (занимает много вертикального пространства):
+
 for item in os.listdir(path='.'):
 	print(item)
 print(divider)
 
-
-
 # Задание 5.
 # Выведите на экран список файлов родительского каталога.
+
 print("5) Вывод списка файлов и папок родительского каталога:")
 print(os.listdir(path='..'))
 
@@ -61,6 +59,7 @@ print(os.listdir(path='..'))
 # Модуль os работает в разных средах по разному и по уму-то нужно
 # писать сначала проверку на тип системы и отдельные куски кода под
 # конкретный вариант.
+
 print(divider)
 
 # Задание 6.
@@ -71,15 +70,18 @@ print(divider)
 import multiprocessing, time, random
 
 def count():
-	time.sleep(random.randint(1, 5))
-	print(datetime.now(), '\n')
+	time.sleep(random.randint(1, 2))
+	print("Counter:", datetime.now(), '\n')
 	#вывод потоков будет в самом конце консоли
 
 print("6) Вывод через multiprocessing:")
 
-for i in range(3):
-	proc = multiprocessing.Process(target=count)
-	proc.start()
+if __name__ == '__main__':
+	for i in range(3):
+		proc = multiprocessing.Process(target=count)
+		proc.start()
+		time.sleep(10)
+		proc.terminate()
 
 print(divider)
 
@@ -92,11 +94,11 @@ birthday = date(1956, 1, 31)
 print("7) Дата рождения Гвидо Ван Россума:", birthday)
 print(divider)
 
-
 # Задание 8.
 # В какой день недели вы родились?
 
 import calendar
+from datetime import timedelta
 
 print("8) День рождения Гвидо Ван Россума:",calendar.day_name[birthday.weekday()])
 print(divider)
@@ -104,8 +106,6 @@ print(divider)
 # Задание 9.
 # Когда вам будет (или уже было) 10 000 дней от роду?
 
-days = 10000
-inc_days = int(birthday.strftime('%s')) + (days*86400)
-print("9) День, когда Гвидо Ван Россуму будет 10 000 дней от роду:", datetime.fromtimestamp(inc_days))
-
+inc_days = birthday + timedelta(days=10000)
+print("9) День, когда Гвидо Ван Россуму будет 10 000 дней от роду:", inc_days)
 
